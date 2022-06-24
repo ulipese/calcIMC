3e22b268e8f8a54fdfa472b17db8b42215732de6
2etimds <2etimds@bglab.local>
2022-06-24 12:07:49 -0300

    Add height comments

--- app/src/main/java/com/example/jaimeesousa/MainActivity.java
+++ app/src/main/java/com/example/jaimeesousa/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText editPeso, editAltura;
     TextView resultText;
+    RadioButton radioFem, radioMasc;
     double peso, altura;
 
     @Override
@@ -20,23 +22,41 @@ public class MainActivity extends AppCompatActivity {
         editPeso = findViewById(R.id.inputPeso);
         editAltura = findViewById(R.id.inputAltura);
         resultText = findViewById(R.id.result);
+        radioFem = findViewById(R.id.radioF);
+        radioMasc = findViewById(R.id.radioM);
     }
     public void CalcularIMC (View view) {
         peso =  Double.parseDouble(editPeso.getText().toString());
         altura = Double.parseDouble(editAltura.getText().toString());
         double result = Math.round(peso / (Math.pow(altura, 2)));
 
-        if (result < 18.5) {
-            resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está abaixo do peso.");
+        if (radioFem.isChecked()) {
+            if (result < 18.5) {
+                resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está abaixo do peso.");
+            }
+            if (result >= 18.6 && result < 25) {
+                resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está no peso ideal.");
+            }
+            if (result >= 25 && result < 30) {
+                resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está pouco acima do peso ideal.");
+            }
+            if (result >= 30) {
+                resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está muito acima do peso ideal.");
+            }
         }
-        if (result >= 18.6 && result < 25) {
-            resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está no peso ideal.");
-        }
-        if (result >= 25 && result < 30) {
-            resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está pouco acima do peso ideal.");
-        }
-        if (result >= 30) {
-            resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está muito acima do peso ideal.");
+        if (radioMasc.isChecked()) {
+            if (result < 18.5) {
+                resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está abaixo do peso.");
+            }
+            if (result >= 18.6 && result < 25) {
+                resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está no peso ideal.");
+            }
+            if (result >= 25 && result < 30) {
+                resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está pouco acima do peso ideal.");
+            }
+            if (result >= 30) {
+                resultText.setText("O seu IMC é: " + Double.valueOf(result).toString() + ", você está muito acima do peso ideal.");
+            }
         }
     }
 }
